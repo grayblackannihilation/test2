b803706653536413b5005313704272014daf4398
era3effulgence <123359284+grayblackannihilation@users.noreply.github.com>
2023-02-06 21:44:25 +0800

    1

--- js/plugins.js
+++ js/plugins.js
@@ -6,7 +6,7 @@ var $plugins =
 {"name":"ZZZ_JumpToDestination","status":true,"description":"(v1.1)瞬移至目的地","parameters":{"瞬移模式的绑定的开关id":"1"}},
 {"name":"PY_AutoSave","status":true,"description":"(v1.0)这是一款可以自动存档的插件","parameters":{"存档位置":"1","存档音效":""}},
 {"name":"ENS_fileload","status":false,"description":"文件路径读取 Ver 1.0","parameters":{"src":"D:\\RPGMV\\怪談"}},
-{"name":"ENS_Loading","status":true,"description":"游戏加载 Ver 1.0","parameters":{}},
+{"name":"ENS_Loading","status":false,"description":"游戏加载 Ver 1.0","parameters":{}},
 {"name":"windows","status":true,"description":"","parameters":{}},
 {"name":"RS_Window_KorNameEdit","status":true,"description":"This plugin allows you to type in korean in the Name Input Proccessing <RS_Window_KorNameEdit>","parameters":{"windowWidth":"auto","windowCenter":"false","editWindow_Opacity":"225","helpWindow_Opacity":"225","askingText":"使用鍵盤輸入","outlineWidth":"1","outlineColor":"black","fontColor":"white","standardFontSize":"28","Chinese Fonts":"NotoSansTC-Regular, Heiti TC, sans-serif, Noto Sans","Korean Fonts":"NotoSansTC-Regular, Dotum, AppleGothic, sans-serif","Default Fonts":"GameFont","Default CharWidth":"A","Default Background":"auto","Default Edit Button":"鍵盤","Default OK Button":"確定","Default Cancel Button":"取消","Error Message":"","didnt_type_anytext":"Cannot an empty name","cant_type_same_name":"Cannot set as the same name","Show Error Message":"false","Keyboard Editor Hidden":"true","Style":"{\"width\":\"60%\",\"textIndent\":\"10px\",\"fontSize\":\"16px\",\"lineHeight\":\"120%\",\"border\":\"3px solid #bd7419\",\"cursor\":\"text\",\"Position\":\"\",\"top\":\"\\\"\\\"\",\"left\":\"\\\"\\\"\",\"right\":\"0\",\"bottom\":\"0\"}","Show Face":"true"}},
 {"name":"對話居中","status":true,"description":"","parameters":{}},
