01a6a1b84c3a6de50fbc38ede5376d41f42b5910
era3effulgence <123359284+grayblackannihilation@users.noreply.github.com>
2023-04-25 18:32:03 +0800

    1

--- js/plugins/windows.js
+++
@@ -1,4 +0,0 @@
-SceneManager._screenWidth = window.innerWidth;
-SceneManager._screenHeight = window.innerHeight;
-SceneManager._boxWidth = window.innerWidth;
-SceneManager._boxHeight = window.innerHeight;
