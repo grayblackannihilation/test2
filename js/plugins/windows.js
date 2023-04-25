7e8e8512e396ad75458826af1d13fe412191b04a
era3effulgence <123359284+grayblackannihilation@users.noreply.github.com>
2023-04-25 20:00:09 +0800

    1

--- js/plugins/windows.js
+++ js/plugins/windows.js
@@ -3,14 +3,3 @@ SceneManager._screenHeight      = 912;
 SceneManager._boxWidth          = 720;
 SceneManager._boxHeight         = 912;
 //一格48*48
-Graphics._updateAllElements = function() {
-    this._updateRealScale();
-    this._updateErrorPrinter();
-    this._updateCanvas();
-    this._updateVideo();
-    this._updateUpperCanvas();
-    this._updateRenderer();
-    this._updateFilterArea();
-    this._updateScreenSprites();
-    this._onWindowResize(); //新增這行程式碼
-};
