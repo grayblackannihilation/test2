b4ea353df87507461b8d93897386d1962f085163
era3effulgence <123359284+grayblackannihilation@users.noreply.github.com>
2023-04-25 18:53:57 +0800

    1

--- js/plugins/windows.js
+++ js/plugins/windows.js
@@ -1,1 +1,7 @@
+SceneManager._screenWidth       = 720;
+SceneManager._screenHeight      = 912;
+SceneManager._boxWidth          = 720;
+SceneManager._boxHeight         = 912;
+//一格48*48
+
 Graphics._requestFullScreen();
